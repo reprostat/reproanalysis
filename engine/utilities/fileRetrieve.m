f52d73b6843cd04695d5eef4e7cb946550b2a440
tiborauer <tibor.auer@gmail.com>
2023-01-12 21:58:19 +0000

    TYPO

--- engine/utilities/fileRetrieve.m
+++ engine/utilities/fileRetrieve.m
@@ -10,6 +10,6 @@ function isFound = fileRetrieve(fname,maximumretry)
                 pause(1);
             end
         end
-        if ~isFound, logging.error('Could not find %s - Are you sure it is on your path?', fname); end
+        if ~isFound, logging.error('Could not find %s - Are you sure it is in your path?', fname); end
     end
 end
