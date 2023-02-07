d829e94fb1f09cc9990e90e75613fcb4711e6ea1
tiborauer <tibor.auer@gmail.com>
2023-02-07 11:45:38 +0000

    FIX (part1) - camelCase

--- engine/utilities/websave.m
+++
@@ -1,8 +0,0 @@
-function fname = websave(fname,url,options)
-    fid = fopen(fname,'w');
-    fputs(fid,webread(url,options));
-    fclose(fid);
-    jf = javaObject('java.io.File',fname);
-    if ~jf.isAbsolute(), fname = fullfile(pwd,fname); end
-end
-
