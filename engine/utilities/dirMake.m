159550bb78f76d4c7420ed3ce7b1eb8adc542a65
tiborauer <tibor.auer@gmail.com>
2023-02-17 22:36:33 +0000

    FIX - dirMake log

--- engine/utilities/dirMake.m
+++ engine/utilities/dirMake.m
@@ -1,14 +1,15 @@
 % checks a directory is made - if not makes it
 
-function [resp mkdirMsg] = dirMake(dirname)
+function [resp, mkdirMsg] = dirMake(dirname)
     if ~exist(dirname,'dir')
         try
-           [mkdirStatus, mkdirMsg] = mkdir(dirname);
+           [~, mkdirMsg] = mkdir(dirname);
         catch
-            logging.error(true,sprintf('Problem making directory %s',dirname));
+            logging.error('Problem making directory %s',dirname);
         end
         resp = true; % created
     else
         resp = false; % not created
+        mkdirMsg = sprintf('%s already exists',dirname);
     end
 end
