09f44ea16d411a118f7274bef288450bb4039486
tiborauer <tibor.auer@gmail.com>
2023-02-17 17:33:12 +0000

    ENH - reporting multiple media at once

--- engine/report/addReportMedia.m
+++ engine/report/addReportMedia.m
@@ -1,39 +1,56 @@
 % Scaling - 0: do not scale, 1: autoscale to defWIDTH, [0< <1]: autoscale to scale*defWIDTH
-function rap = addReportMedia(rap,reportStore,fnMedium,scaling)
+function rap = addReportMedia(rap,reportStore,fnAll,varargin)
 
-defWIDTH=1600;
+    defWIDTH=1600;
 
-if nargin < 4, scaling = 1; end;
+    argParse = inputParser;
+    argParse.addParameter('scaling',1,@isnumeric);
+    argParse.addParameter('displayFileName',1,@(x) isnumeric(x) | islogical(x));
+    argParse.parse(varargin{:});
+    scaling = argParse.Results.scaling;
 
-if ~startsWith(fnMedium,rap.acqdetails.root)
-    logging.error('Cannot relate file %s to directory root %s',fnMedium,rap.acqdetails.root);
-end
+    fnAll = cellstr(fnAll);
+    nImg = numel(fnAll) - sum(endsWith(fnAll,{'.avi' '.mp4'}));
 
-[~, baseFn, ext] = fileparts(fnMedium);
-switch ext
-    case {'.png', '.jpg', '.jpeg'}
-        reportStr = sprintf('<cite>%s</cite><img src',baseFn);
-        repClose = '><br>';
-        mediumSize = size(imread(fnMedium));
-        if scaling == 1 && (mediumSize(2) <= defWIDTH), scaling = 0; end
-    case {'.avi' '.mp4'}
-        reportStr = '<a href';
-        repClose = sprintf('>Play video: %s</a><br>',baseFn);
-        scaling = 0;
-    otherwise
-        logging.error('Unknown format: %s',e);
-end
+    if nImg > 1, addReport(rap,reportStore,'<table><tr>'); end
 
-switch scaling
-    case 1
-        reportStr = [reportStr '="' fnMedium '" ' sprintf('width=%d height=%d',defWIDTH,round(defWIDTH/mediumSize(2)*mediumSize(1))) repClose];
-    case 0
-        reportStr = [reportStr '="' fnMedium '"' repClose];
-    otherwise
-        reportStr = [reportStr '="' fnMedium '" ' sprintf('width=%d height=%d',round(mediumSize(2)*scaling),round(mediumSize(1)*scaling)) repClose];
-end
+    for fnMedium = reshape(fnAll,1,[])
+        if ~startsWith(fnMedium{1},rap.acqdetails.root)
+            logging.error('Cannot relate file %s to directory root %s',fnMedium{1},rap.acqdetails.root);
+        end
 
-addReport(rap,reportStore,reportStr);
+        [~, baseFn, ext] = fileparts(fnMedium{1});
+        if ~argParse.Results.displayFileName, baseFn = ''; end
+        switch ext
+            case {'.png', '.jpg', '.jpeg'}
+                reportStr = sprintf('<cite>%s</cite><img src',baseFn);
+                repClose = '><br>';
+                mediumSize = size(imread(fnMedium{1}));
+                if scaling == 1 && (mediumSize(2) <= defWIDTH), scaling = 0; end
+            case {'.avi' '.mp4'}
+                reportStr = '<a href';
+                repClose = sprintf('>Play video: %s</a><br>',baseFn);
+                scaling = 0;
+            otherwise
+                logging.error('Unknown format: %s',e);
+        end
 
-rap.report.attachment{end+1} = fnMedium;
+        switch scaling
+            case 1
+                reportStr = [reportStr '="' fnMedium{1} '" ' sprintf('width=%d height=%d',defWIDTH,round(defWIDTH/mediumSize(2)*mediumSize(1))) repClose];
+            case 0
+                reportStr = [reportStr '="' fnMedium{1} '"' repClose];
+            otherwise
+                scaling = argParse.Results.scaling*defWIDTH/mediumSize(2);
+                reportStr = [reportStr '="' fnMedium{1} '" ' sprintf('width=%d height=%d',round(mediumSize(2)*scaling),round(mediumSize(1)*scaling)) repClose];
+        end
 
+        % Video MUST belong to the previous image -> no new table item
+        if nImg > 1 && ~startsWith(reportStr,'<a href'), reportStr = ['<td>' reportStr '</td>']; end
+        addReport(rap,reportStore,reportStr);
+
+        rap.report.attachment(end+1) = fnMedium;
+    end
+
+    if nImg > 1, addReport(rap,reportStore,'</tr></table>'); end
+ end
