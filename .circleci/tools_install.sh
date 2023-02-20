f773e4e514abd56999afe45095def70dcc55158d
tiborauer <tibor.auer@gmail.com>
2023-02-20 15:26:59 +0000

    CI - tools

---
+++ .circleci/tools_install.sh
@@ -0,0 +1,26 @@
+TOOLDIR=$HOME/tools
+mkdir $TOOLDIR
+mkdir $TOOLDIR/config
+TEMPLATEDIR=$TOOLDIR/templates
+
+sudo apt-get update
+sudo apt-get install libtinfo5 libtinfo6 dc libxml2-utils
+curl https://bootstrap.pypa.io/pip/2.7/get-pip.py -o $TOOLDIR/get-pip.py
+python $TOOLDIR/get-pip.py --force-reinstall
+
+# All MATLAB tools MUST be installed referred by the parameterset
+source $GITHUB_WORKSPACE/.circleci/tools_urls.sh
+source $GITHUB_WORKSPACE/external/toolboxes/installation_scripts/install_tools.sh $GITHUB_WORKSPACE/.circleci/$PARAMETER_XML $TOOLDIR
+
+echo "FSL: ${LOAD_FSL}; FREESURFER: ${LOAD_FREESURFER}"
+
+if [[ "x${LOAD_FSL}x" == "x1x" ]]; then
+    source $GITHUB_WORKSPACE/external/toolboxes/installation_scripts/install_fsl.sh $TOOLDIR 6.0.5 centos7 0 $TOOLDIR/config/fsl_bash.sh
+fi
+
+if [[ "x${LOAD_FREESURFER}x" == "x1x" ]]; then
+    source $GITHUB_WORKSPACE/external/toolboxes/installation_scripts/install_freesurfer.sh $TOOLDIR 7.2.0 centos7 "tibor.auer@gmail.com\n7061\n *Ccpi6x7PAIeQ\n FS96pPK5vW.0g" $TEMPLATEDIR
+fi
+
+echo "Free space:"
+df -h
---
+++ .circleci/tools_urls.sh
@@ -0,0 +1,8 @@
+# Set of variables (URL_<tool name with lower case>) describing the source of tools
+
+URL_spm=git+https://github.com/spm/spm12
+URL_facemasking=git+https://github.com/mmilch01/mask_face
+URL_aroma=git+https://github.com/maartenmennes/ICA-AROMA
+URL_wls=git+https://github.com/jdiedrichsen/rwls
+URL_eeglab=git+https://github.com/sccn/eeglab+eeglab2021.1
+URL_fieldtrip=git+https://github.com/fieldtrip/fieldtrip
