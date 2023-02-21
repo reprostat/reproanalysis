7ab94601b174cfe8bf256b6a05d92404e7efd75f
tiborauer <tibor.auer@gmail.com>
2023-02-21 22:33:22 +0000

    ENH - loglevel

--- engine/utilities/logging.m
+++ engine/utilities/logging.m
@@ -3,30 +3,31 @@ classdef logging
     methods  (Static = true)
         function this = info(varargin)
             varargin{1} = ['info: ' varargin{1} '\n'];
-            fprintf(varargin{:});
 
+            toLog = true;
             global reproaworker
-            if isa(reproaworker,'workerClass'), reproaworker.addLog(varargin{:}); end
+            if isa(reproaworker,'workerClass'), toLog = reproaworker.addLog(varargin{:}); end
+            if toLog, fprintf(varargin{:}); end
         end
 
         function this = warning(varargin)
+            toLog = true;
             global reproaworker
             if isa(reproaworker,'workerClass')
                 varargin{1} = ['warning: ' varargin{1}];
-                reproaworker.addLog(varargin{:});
+                toLog = reproaworker.addLog(varargin{:});
             end
-
-            warning(sprintf(varargin{:}));
+            if toLog, warning(sprintf(varargin{:})); end
         end
 
         function this = error(varargin)
+            toLog = true;
             global reproaworker
             if isa(reproaworker,'workerClass')
                 varargin{1} = ['error: ' varargin{1}];
-                reproaworker.addLog(varargin{:});
+                toLog = reproaworker.addLog(varargin{:});
             end
-
-            error(sprintf(varargin{:}));
+            if toLog, error(sprintf(varargin{:})); end
         end
     end
 
