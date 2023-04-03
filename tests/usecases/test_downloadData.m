6a2180cb24b787c8be449d8e9e77fe796f718763
tiborauer <tibor.auer@gmail.com>
2023-04-03 09:45:53 +0100

    FIX - test_downloadData target

--- tests/usecases/test_downloadData.m
+++ tests/usecases/test_downloadData.m
@@ -10,15 +10,15 @@ rap.directoryconventions.rawdatadir = fullfile(dataroot, 'ds000114');
 downloadData(rap, 'ds000114',  'sub-01');
 
 %% ds002737
-rap.directoryconventions.rawdatadir = fullfile(dataroot, 'ds000114');
+rap.directoryconventions.rawdatadir = fullfile(dataroot, 'ds002737');
 downloadData(rap, 'ds002737',  'sub-01/ses-03');
 
 %% LEMON_MRI
-rap.directoryconventions.rawdatadir = fullfile(dataroot, 'ds000114');
+rap.directoryconventions.rawdatadir = fullfile(dataroot, 'LEMON_MRI');
 downloadData(rap, 'LEMON_MRI',  'sub-032301/ses-01/anat');
 
 %% LEMON_EEG
-rap.directoryconventions.rawdatadir = fullfile(dataroot, 'ds000114');
+rap.directoryconventions.rawdatadir = fullfile(dataroot, 'LEMON_EEG');
 downloadData(rap, 'LEMON_EEG',  'sub-032301');
 
 %% Report
