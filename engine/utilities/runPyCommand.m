2c644793552e0087eb4163253f6be2cda2dbf827
tiborauer <tibor.auer@gmail.com>
2023-05-18 15:14:16 +0200

    FIX - condasetup

--- engine/utilities/runPyCommand.m
+++ engine/utilities/runPyCommand.m
@@ -1,5 +1,5 @@
 function [s, w] = runPyCommand(rap,pycmd,condaenv,varargin)
-% Requires 
+% Requires
 %   - conda installed and initialised
 %   - separate conda setup file, which can be sourced. You can create by
 %       sed '/>>> conda initialize >>>/,/<<< conda initialize <<</!d' ~/.bashrc >> $HOME/tools/config/conda_bash.sh
@@ -7,7 +7,13 @@ function [s, w] = runPyCommand(rap,pycmd,condaenv,varargin)
 
 
 % Setup
+condasetup = deblank(rap.directoryconventions.condasetup);
+if not(isempty(condasetup))
+    if ~startsWith(condasetup,'. '), condasetup = ['. ' condasetup]; end
+    if ~endsWith(condasetup,';'), condasetup=[condasetup ';']; end
+end
+
 if nargin < 2, condaenv = 'base'; end
 if nargin < 4, varargin = {}; end
 
-[s, w] = shell(pycmd,'shellprefix',['. ' rap.directoryconventions.condasetup 'conda activate ' condaenv ';'],varargin{:});
+[s, w] = shell(pycmd,'shellprefix',[condasetup 'conda activate ' condaenv ';'],varargin{:});
