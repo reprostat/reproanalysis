8a52d62901f147957079b13212ebd5079a67b1c7
tiborauer <tibor.auer@gmail.com>
2023-06-28 12:19:25 +0200

    FIX - DARTEL

--- modules/preprocessing/reproa_dartelcreatetemplate.m
+++ modules/preprocessing/reproa_dartelcreatetemplate.m
@@ -9,10 +9,10 @@ switch command
         % collect images
         imgAll{1} = cell(1,getNByDomain(rap,'subject'));
         imgAll{2} = cell(1,getNByDomain(rap,'subject'));
-        for subjind = 1:getNByDomain(rap,'subject')
-            seg = getFileByStream(rap, 'subject',subjind, 'dartelimported_segmentations','content',{'GM' 'WM'});
-            imgAll{1}(subjind) = seg.GM;
-            imgAll{2}(subjind) = seg.WM;
+        for subj = 1:getNByDomain(rap,'subject')
+            seg = getFileByStream(rap, 'subject',subj, 'dartelimported_segmentations','content',{'GM' 'WM'});
+            imgAll{1}(subj) = seg.GM;
+            imgAll{2}(subj) = seg.WM;
         end
         toExcl = cellfun(@(s) any(strcmp(getSetting(rap,'exclude'),s)), {rap.acqdetails.subjects.subjname});
         imgTemplate{1} = imgAll{1}(~toExcl);
@@ -23,6 +23,12 @@ switch command
         % Set up job
         % - parse defaults
         cfg = tbx_cfg_dartel;
+        % - rearrange spm_mod
+        modDir = fullfile(spm_file(which('spmClass'),'path'),'spm_mods');
+        if exist(modDir,'dir')
+            addpath(modDir);
+        end
+
         cfgReq = {'param' 'optim'};
         cfgTag = cellfun(@(v) v.tag, cfg.values{2}.val{2}.val, 'UniformOutput',false);
         cfgCheck = ismember(cfgReq,cfgTag);
@@ -45,21 +51,19 @@ switch command
                           'param', param,...
                           'optim', optim);
 
-        % create template
+        % Create template
         if ~isempty(imgTemplate{1})
             spm_dartel_template(struct('images', {imgTemplate}, 'settings', settings));
 
-            % (template in first subject)
-            for t = 1:6
-               template(t,:) = spm_select('fplist', fileparts(imgTemplate{1}{1}), sprintf('Template_%d',t));
-            end
+            % - template in first subject
+            template = cellstr(spm_select('FPListRec', getPathByDomain(rap,'subject',1), '^Template_[1-6]\.nii$'));
         end
 
-        % warp excluded subjects
+        % Warp excluded subjects
         if ~isempty(imgNoTemplate{1})
             settings = rmfield(settings,'template');
             for t = 1:6
-               settings.param(t).template = {template(t,:)};
+               settings.param(t).template = template(t);
             end
             out = spm_dartel_warp(struct('images', {imgNoTemplate}, 'settings', settings));
             for f = 1:numel(out.files)
@@ -67,38 +71,27 @@ switch command
             end
         end
 
-        % template
-        rap = aas_desc_outputs(rap, 'dartel_template', template(6,:));
-
-        % normalisation XFM
-        fname_template = aas_getfiles_bystream(rap,'study',[],'dartel_template','output');
-        stages = fieldnames(rap.tasksettings);
-        tpm = rap.tasksettings.(stages{find(cellfun(@(x) ~isempty(regexp(x,'^aamod_segment.*','match')), stages),1,'first')}).tpm;
-        affine = spm_get_space(tpm)/spm_klaff(nifti(fname_template),tpm);
-        xfm = affine/spm_get_space(fname_template);
-        fname_xfm = fullfile(aas_getstudypath(rap),'dartel_templatetomni_xfm.mat');
-        save(fname_xfm,'xfm')
-        rap = aas_desc_outputs(rap, 'dartel_templatetomni_xfm', fname_xfm);
-
-        % normalised template
-        Vt = spm_file_split(spm_vol(fname_template),aas_getstudypath(rap)); % --> Template_6-0_00001.nii (GM) and Template_6-0_00002.nii (WM)
-
-        spm_get_space(Vt(1).fname,xfm*spm_get_space(Vt(1).fname));
-        fname_ngrey = spm_file(Vt(1).fname,'prefix',rap.spm.defaults.normalise.write.prefix);
-        spm_smooth(Vt(1).fname,fname_ngrey,[0 0 0]);
-        rap = aas_desc_outputs(rap, 'normalised_dartel_template_grey', fname_ngrey);
-
-        spm_get_space(Vt(2).fname,xfm*spm_get_space(Vt(2).fname));
-        fname_nwhite = spm_file(Vt(2).fname,'prefix',rap.spm.defaults.normalise.write.prefix);
-        spm_smooth(Vt(2).fname,fname_nwhite,[0 0 0]);
-        rap = aas_desc_outputs(rap, 'normalised_dartel_template_white', fname_nwhite);
-
-        % flow fields
-        for subjind = 1:length(rap.acqdetails.subjects)
-            pth = fileparts(imgAll{1}{subjind});
-            flowimg = spm_select('fplist', pth, '^u_');
-            rap = aas_desc_outputs(rap, subjind, 'dartel_flowfield', flowimg);
+        % Output
+        % - flow fields
+        for subj = 1:getNByDomain(rap,'subject')
+            putFileByStream(rap, 'subject',subj, 'dartelflowfield', ...
+                            spm_select('FPListRec', getPathByDomain(rap,'subject',subj), '^u_.*_Template\.nii$'));
         end
+
+        % - template
+        templateToStream = spm_file(template{6},'path',getPathByDomain(rap,'study',[]));
+        movefile(template{6},templateToStream);
+        putFileByStream(rap, 'study',[], 'darteltemplate', templateToStream);
+
+        % - normalisation XFM
+        stages = fieldnames(rap.tasksettings);
+        tpm = getSetting(setCurrenttask(rap,'task',getSourceTaskInd(rap,'reproa_segment')),'segmentation.tpm');
+        affine = spm_get_space(tpm)/spm_klaff(nifti(templateToStream),tpm);
+        xfm = affine/spm_get_space(templateToStream);
+        fnXFM = fullfile(getPathByDomain(rap,'study',[]),'dartel_templatetomni_xfm.mat');
+        save(fnXFM,'xfm')
+        putFileByStream(rap, 'study',[], 'darteltemplatetomni', fnXFM);
+
     case 'checkrequirements'
         if hasStream(rap,'study',[],'darteltemplate')
             logging.info('External template will be used');
