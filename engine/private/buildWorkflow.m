e22f641dac743d1b5596a68752c5b5ee3508043d
tiborauer <tibor.auer@gmail.com>
2023-06-30 12:15:33 +0200

    FIX - workflow with renamed output

--- engine/private/buildWorkflow.m
+++ engine/private/buildWorkflow.m
@@ -43,7 +43,9 @@ function rap = buildWorkflow(rap,varargin)
             if any(inputstream.name == '.'), inputstream.name = regexp(inputstream.name,'^.*(?=\.)','match','once'); end
 
             if any(sourceToCheck)
-                indSource = find(arrayfun(@(i) sourceToCheck(i) && ~isempty(rap.tasklist.main(i).outputstreams) && any(strcmp(cellstr({rap.tasklist.main(i).outputstreams.name}),inputstream.name)), 1:indTask-1),1,'last');
+
+
+                indSource = find(arrayfun(@(i) sourceToCheck(i) && ~isempty(rap.tasklist.main(i).outputstreams) && any(arrayfun(@(s) any(strcmp(s.name,inputstream.name)),rap.tasklist.main(i).outputstreams)), 1:indTask-1),1,'last');
             end
 
             if isempty(indSource) && ~isempty(rap.acqdetails.input.remotepipeline(1).path) % Check remote
