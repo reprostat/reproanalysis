61bc8f0aeca3599e53cab4d26a17286d032c3652
tiborauer <tibor.auer@gmail.com>
2023-07-05 21:11:55 +0100

    update submodule octave-pool

--- engine/utilities/isAbsolutePath.m
+++
@@ -1,8 +0,0 @@
-function resp = isAbsolutePath(pth)
-    if isOctave
-        resp = is_absolute_filename(pth);
-    else
-        jvFile = java.io.File(pth);
-        resp = jvFile.isAbsolute();
-    end
-end
