f9432091e7b96a62e6517f36d3e819de4c519350
tiborauer <tibor.auer@gmail.com>
2023-09-15 17:36:50 +0100

    FIX - CI: docker

--- .circleci/tools_install_ubuntu.sh
+++ .circleci/tools_install_ubuntu.sh
@@ -4,22 +4,22 @@ mkdir $TOOLDIR/config
 TEMPLATEDIR=$TOOLDIR/templates
 
 sudo apt update
-sudo apt install libtinfo5 libtinfo6 dc libxml2-utils graphviz
+sudo apt install libtinfo5 libtinfo6 dc libxml2-utils graphviz -y
 curl https://bootstrap.pypa.io/pip/2.7/get-pip.py -o $TOOLDIR/get-pip.py
 python $TOOLDIR/get-pip.py --force-reinstall
 
 # All MATLAB tools MUST be installed referred by the parameterset
 source $GITHUB_WORKSPACE/.circleci/tools_urls.sh
-source $GITHUB_WORKSPACE/external/toolboxes/installation_scripts/install_tools.sh $GITHUB_WORKSPACE/.circleci/$PARAMETER_XML $TOOLDIR
+source $GITHUB_WORKSPACE/external/toolboxes/installation_scripts/install_tools.sh $GITHUB_WORKSPACE/.circleci/$PARAMETER_XML
 
 echo "FSL: ${LOAD_FSL}; FREESURFER: ${LOAD_FREESURFER}"
 
 if [[ "x${LOAD_FSL}x" == "x1x" ]]; then
-    source $GITHUB_WORKSPACE/external/toolboxes/installation_scripts/install_fsl.sh $TOOLDIR 6.0.6.2 0 $TOOLDIR/config/fsl_bash.sh
+    source $GITHUB_WORKSPACE/external/toolboxes/installation_scripts/install_fsl.sh $TOOLDIR 6.0.7.3 0 $TOOLDIR/config/fsl_bash.sh
 fi
 
 if [[ "x${LOAD_FREESURFER}x" == "x1x" ]]; then
-    source $GITHUB_WORKSPACE/external/toolboxes/installation_scripts/install_freesurfer.sh $TOOLDIR 7.3.2 centos7 "tibor.auer@gmail.com\n7061\n *Ccpi6x7PAIeQ\n FS96pPK5vW.0g" $TEMPLATEDIR
+    source $GITHUB_WORKSPACE/external/toolboxes/installation_scripts/install_freesurfer.sh $TOOLDIR 7.4.1 centos7 "tibor.auer@gmail.com\n7061\n *Ccpi6x7PAIeQ\n FS96pPK5vW.0g" $TEMPLATEDIR
 fi
 
 echo "Free space:"
