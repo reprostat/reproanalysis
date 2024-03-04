dde206944cf42ece8a9149a6daf90d0cfa36d91e
tiborauer <tibor.auer@gmail.com>
2024-03-04 13:26:57 +0000

    ENH - getStreamByName provides either origina or current names

--- engine/utilities/getStreamByName.m
+++ engine/utilities/getStreamByName.m
@@ -1,8 +1,15 @@
-function resp = getStreamByName(rap,whatToReturn) % 'streamList', <stream name>
+function resp = getStreamByName(rap,whatToReturn) % '[orig:]streamList', [orig:]<stream name>
     if isOctave(), rePattern= '(?<=\.)?\w*$';
     else, rePattern = '(?<=\.?)\w*$';
     end
-    inStreamList = cellfun(@(n) regexp(n{1},rePattern,'match','once'),...
+
+    indStream = 'first'; % new name if renamed
+    if startsWith(whatToReturn,'orig:')
+        indStream = 'last'; % original name
+        whatToReturn = strrep(whatToReturn,'orig:','');
+    end
+
+    inStreamList = cellfun(@(n) regexp(char(getItem(n,indStream)),rePattern,'match','once'),...
                  arrayfun(@(s) cellstr(s.name),rap.tasklist.currenttask.inputstreams, 'UniformOutput',false),...
                  'UniformOutput',false);
     switch whatToReturn
@@ -10,5 +17,14 @@ function resp = getStreamByName(rap,whatToReturn) % 'streamList', <stream name>
             resp = inStreamList;
         otherwise
             resp = rap.tasklist.currenttask.inputstreams(strcmp(inStreamList,whatToReturn));
+    end
+end
+
+function val = getItem(items,n)
+    switch n
+        case 'first'
+            val = items(1);
+        case 'last'
+            val = items(end);
     end
 end
